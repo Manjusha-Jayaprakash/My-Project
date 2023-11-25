76bdc063067edebcc83b7d636dd645abf4bcbd9a
Manjusha-Jayaprakash <137990906+Manjusha-Jayaprakash@users.noreply.github.com>
2023-11-25 15:40:26 +0530

    Delete src/customers directory

--- src/customers/Customergroups.jsx
+++
@@ -1,7 +0,0 @@
-import { Container } from "react-bootstrap";
-
-export default function Customergroups(){
-    return(
-        <Container className="Customergroups">Customer groups</Container>
-    )
-}

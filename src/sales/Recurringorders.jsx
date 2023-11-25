c5434c92fadb1be2af3cbd1fe0b9b30c208c4b43
Manjusha-Jayaprakash <137990906+Manjusha-Jayaprakash@users.noreply.github.com>
2023-11-25 15:41:07 +0530

    Delete src directory

--- src/sales/Recurringorders.jsx
+++
@@ -1,7 +0,0 @@
-import { Container } from "react-bootstrap";
-
-export default function Recurringorders(){
-    return(
-        <Container className="Recurringorders">Recurring orders</Container>
-    )
-}
